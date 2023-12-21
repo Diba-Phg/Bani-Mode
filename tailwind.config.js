e3f0a68e13936bac72c83e3098c6e6b72d749802
Diba <dibap07@gmail.com>
2023-12-21 21:05:03 +0330

    version 1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      IranSans: ["iransans"],
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
